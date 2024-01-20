84fc3181ec2c6e2d01a8ec57b683b1d2a6b5dc94
wangzhenhao <1471047476@qq.com>
2024-01-20 23:24:54 +0800

    feat: 调试wxss代码压缩

---
+++ src/lib/clean-css/index.js
@@ -0,0 +1,75 @@
+var tokenize = require("./tokenizer/tokenize");
+
+function isTracking(maps, source) {
+    return source in maps;
+}
+
+function inputSourceMapTracker() {
+    var maps = {};
+
+    return {
+        isTracking: isTracking.bind(null, maps),
+        originalPositionFor: originalPositionFor.bind(null, maps),
+    };
+}
+function originalPositionFor(maps, metadata, range, selectorFallbacks) {
+    var line = metadata[0];
+    var column = metadata[1];
+    var source = metadata[2];
+    var position = {
+        line: line,
+        column: column + range,
+    };
+    var originalPosition;
+
+    while (!originalPosition && position.column > column) {
+        position.column--;
+        originalPosition = maps[source].originalPositionFor(position);
+    }
+
+    if (!originalPosition || originalPosition.column < 0) {
+        return metadata;
+    }
+
+    if (originalPosition.line === null && line > 1 && selectorFallbacks > 0) {
+        return originalPositionFor(
+            maps,
+            [line - 1, column, source],
+            range,
+            selectorFallbacks - 1
+        );
+    }
+
+    return originalPosition.line !== null
+        ? toMetadata(originalPosition)
+        : metadata;
+}
+
+function toMetadata(asHash) {
+    return [asHash.line, asHash.column, asHash.source];
+}
+function minifyCss(input) {
+    var context = {
+        stats: {
+            efficiency: 0,
+            minifiedSize: 0,
+            originalSize: 0,
+            startedAt: Date.now(),
+            timeSpent: 0,
+        },
+        cache: { specificity: {} },
+        errors: [],
+        inlinedStylesheets: [],
+        inputSourceMapTracker: inputSourceMapTracker(),
+        // localOnly: !callback,
+        options: {},
+        source: null,
+        sourcesContent: {},
+        // validator: validator(options.compatibility),
+        warnings: [],
+    };
+    const token = tokenize(input, context);
+    console.log(token)
+}
+
+module.exports = minifyCss;
--- test/minifycss.js
+++ test/minifycss.js
@@ -1,4 +1,5 @@
 var CleanCSS = require('clean-css');
+var minifyCss = require('../src/lib/clean-css/index.js')
 
 // const css = `
 // @import "../commont.wxss"
@@ -6,27 +7,34 @@ var CleanCSS = require('clean-css');
 //     background: red;
 //     font-size: 12rpx;
 // }`
-const css = `@import '../commont.wxss'; .test{ font-size: 12px; } @font-face {
+const css = `@import '../commont.wxss'; a .test{ font-size: 12px; } @font-face {
     font-display: auto;
     font-family: vant-icon;
     font-style: normal;
     font-weight: 400;
     src: url(//at.alicdn.com/t/c/font_2553510_kfwma2yq1rs.woff2?t=1694918397022)
             format("woff2"),
-        url(//at.alicdn.com/t/c/font_2553510_kfwma2yq1rs.woff?t=1694918397022)
+        url("//at.alicdn.com/t/c/font_2553510_kfwma2yq1rs.woff?t=1694918397022")
             format("woff");
-}`
-var re = /(\/\/.*\n?)|(\/\*[\s\S]*?\*\/)/g;
-var re2 = /\/\*[\s\S]*?\*\/|(?<=[^:])\/\/.*|^\/\/.*/g
-function minify(code) {
-    // var result = code
-    //     .replace(/( [^0-9a-zA-Z\\.#])\\s+/g, "$1")
-    //     .replace(/\\s ( [^0-9a-zA-Z\\.#]+)/g, "$1")
-    //     .replace(/;}/g, "}")
-    //     .replace(/\/\*.*?\*\//g, "");
-    const result =  code.replace(re2, '')
-    return result;
 }
+// 测试
+
+/**
+ * 测试
+ * /
+`
+// var re = /(\/\/.*\n?)|(\/\*[\s\S]*?\*\/)/g;
+// var re2 = /\/\*[\s\S]*?\*\/|(?<=[^:])\/\/.*|^\/\/.*/g
+// function minify(code) {
+//     // var result = code
+//     //     .replace(/( [^0-9a-zA-Z\\.#])\\s+/g, "$1")
+//     //     .replace(/\\s ( [^0-9a-zA-Z\\.#]+)/g, "$1")
+//     //     .replace(/;}/g, "}")
+//     //     .replace(/\/\*.*?\*\//g, "");
+//     const result =  code.replace(re2, '')
+//     return result;
+// }
 
 // console.log(new CleanCSS({inline: 'none' }).minify(css))
-console.log(minify(css))
+console.log(minifyCss(css))
+// console.log(minify(css))
